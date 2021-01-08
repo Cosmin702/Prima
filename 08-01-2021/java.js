f9c4dc73cc1b405b0e9ca5b08962fa4f01ab0ccc
Cosmin702 <75899266+Cosmin702@users.noreply.github.com>
2021-01-08 13:13:43 +0100

    8-1-2021 javascript

---
+++ 08-01-2021/java.js
@@ -0,0 +1,5 @@
+var lato1=parseInt(prompt("Inserisci lato 1"))
+var lato2=parseInt(prompt("Inserisci lato 2"))
+var lato3=parseInt(prompt("Inserisci lato 3"))
+perimetro = lato1 + lato2 + lato3
+alert ("perimetro = " + perimetro)
